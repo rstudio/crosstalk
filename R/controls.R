fa4d2d1dc41b7a90430d73028d33cf6a94f96130
JohnCoene <jcoenep@gmail.com>
2021-06-15 17:47:36 +0200

    remove erroneous 'deprecated' device

--- R/controls.R
+++ R/controls.R
@@ -484,7 +484,7 @@ animation_options <- function(interval=1000,
 #'   The width vector will be recycled if there are more \code{...} arguments.
 #'   \code{NA} columns will evenly split the remaining columns that are left
 #'   after the widths are recycled and non-\code{NA} values are subtracted.
-#' @param device Deprecated. The class of device which is targeted by these widths; with
+#' @param device The class of device which is targeted by these widths; with
 #'   smaller screen sizes the layout will collapse to a one-column,
 #'   top-to-bottom display instead. xs: never collapse, sm: collapse below
 #'   768px, md: 992px, lg: 1200px.

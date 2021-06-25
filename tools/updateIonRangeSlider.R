58e141f15b15027161cdd9a51af20898657a7ecc
Carson <cpsievert1@gmail.com>
2021-06-25 12:23:38 -0500

    Default to main branch

--- tools/updateIonRangeSlider.R
+++ tools/updateIonRangeSlider.R
@@ -1,7 +1,7 @@
 pkg_root <- rprojroot::find_package_root_file()
 
 # Version of shiny to use when copying over ionrangeslider assets
-version <- "v1.6.0"
+version <- "master"
 withr::with_tempdir({
   cmd <- paste("git clone --depth 1 --branch", version, "https://github.com/rstudio/shiny")
   system(cmd)

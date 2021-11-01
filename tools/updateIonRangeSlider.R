80ee1cb45d019b92ed58c156b037b9d2b9317ddb
Barret Schloerke <barret@rstudio.com>
2021-11-01 10:09:04 -0400

    master -> main (#112)

--- tools/updateIonRangeSlider.R
+++ tools/updateIonRangeSlider.R
@@ -2,7 +2,7 @@ lib_dir <- rprojroot::find_package_root_file("inst/lib")
 unlink(file.path(lib_dir, "ionrangeslider"), recursive = TRUE)
 
 # Version of shiny to use when copying over ionrangeslider assets
-version <- "master"
+version <- "main"
 withr::with_tempdir({
   cmd <- paste("git clone --depth 1 --branch", version, "https://github.com/rstudio/shiny")
   system(cmd)

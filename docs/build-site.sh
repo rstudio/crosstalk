f8bb92d0cd52eb2b0cad7ce3abb31511f8aff3c0
Garrick Aden-Buie <garrick@adenbuie.com>
2025-01-08 16:24:37 -0500

    chore: fully clean site

--- docs/build-site.sh
+++ docs/build-site.sh
@@ -5,4 +5,8 @@ set -e
 (cd .. && node node_modules/.bin/jsdoc2md javascript/src/filter.js | sed -e 's/ --- / ------------- /g' | sed -e 's/exports.FilterHandle/crosstalk.FilterHandle/' > docs/filter.Rmd)
 (cd .. && node node_modules/.bin/jsdoc2md javascript/src/selection.js | sed -e 's/ --- / ------------- /g' | sed -e 's/exports.SelectionHandle/crosstalk.SelectionHandle/' > docs/selection.Rmd)
 
-R --vanilla --no-echo -e 'set.seed(100); rmarkdown::clean_site("."); rmarkdown::render_site(".")'
+R --vanilla --no-echo << 'EOF'
+set.seed(100)
+rmarkdown::clean_site(".", preview = FALSE)
+rmarkdown::render_site(".")
+EOF
